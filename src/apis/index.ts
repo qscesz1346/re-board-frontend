59f0e1055d443dbe25a902abf53259a2a0c5db3e
qscesz1346 <qscesz1346@naver.com>
2024-02-06 02:21:10 +0900

    1

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -8,7 +8,7 @@ import { PostBoardResponseDto, GetBoardResponseDto, IncreaseViewCountResponseDto
 import { GetPopularListResponseDto, GetRelationListResponseDto } from './response/search';
 import { PatchNicknameRequestDto, PatchProfileImageRequestDto } from './request/user';
 
-const DOMAIN = 'http://localhost:4000';
+const DOMAIN = '';
 
 const API_DOMAIN = `${DOMAIN}/api/v1`;
 
